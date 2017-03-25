6137abfad8283841db7307232803786293e388b2
jeevanshinde <kjeevan272@gmail.com>
2017-03-25 16:25:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/three', function (req, res) {
+  res.send(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/two', function (req, res) {
+  res.send(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/one', function (req, res) {
+  res.send(path.join(__dirname, 'ui', 'index.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
